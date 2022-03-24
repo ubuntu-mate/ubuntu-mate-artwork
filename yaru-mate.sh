16b767ae06cc6096cd09f4ac83591a7a9da3c66d
Martin Wimpress <code@wimpress.io>
2022-03-24 13:04:11 +0000

    Update yaru-mate.sh

--- yaru-mate.sh
+++ yaru-mate.sh
@@ -39,13 +39,10 @@ if [ -d build ]; then
     rm -rfv build
 fi
 
-# Revert making panel dark in both light and dark themes.
-#  - https://github.com/ubuntu/yaru/pull/3378
-#  - https://github.com/ubuntu/yaru/issues/3307
-#git revert --no-edit --no-commit 85312a3338b21d760ca79b00d87c1e3597262708
-wget -q "https://patch-diff.githubusercontent.com/raw/ubuntu/yaru/pull/3430.diff" -O 3430.diff
-patch -p1 < 3430.diff
-rm 3430.diff
+git checkout 4fb027d2f83cb5b6d7b254641d6d3131832e78b7
+wget -nc "https://patch-diff.githubusercontent.com/raw/ubuntu/yaru/pull/3545.diff" -O 3545.diff
+patch -p1 < 3545.diff
+rm 3545.diff
 
 # Enable MATE themes
 sed  -i "s|mate', type: 'boolean', value: false|mate', type: 'boolean', value: true|" meson_options.txt
@@ -53,75 +50,188 @@ sed  -i "s|mate-dark', type: 'boolean', value: false|mate-dark', type: 'boolean'
 
 rm -rf "${YARU_NEW}"
 mkdir -p "${YARU_NEW}"
+
+#gtk-2.0
+# Hover:    SAT-10    or VAL+5
+# Activ:    HUE+10
+# Insen:    SAT=20    or VAL+5 SAT=10
+# Insen Dk: VAL=30
+# for THEME in bark blue magenta mate olive prussiangreen purple red sage viridian; do
+#   case ${THEME} in
+#     bark)
+#         accent="#787859"
+#         hover="#858562"
+#         active="#737859"
+#         insensitive="#858577"
+#         insensitive_dark="#4D4D39"
+#         ;;
+#     blue)
+#         accent="#0073E5"
+#         hover="#177EE5"
+#         active="#004CE5"
+#         insensitive="#B8CFE6"
+#         insensitive_dark="#00264D"
+#         ;;
+#     magenta)
+#         accent="#B34CB3"
+#         hover="#B35DB3"
+#         active="#B34CA2"
+#         insensitive="#B38FB3"
+#         insensitive_dark="#4D204D"
+#         ;;
+#     mate)
+#         accent="#87A556"
+#         hover="#8DA566"
+#         active="#7AA556"
+#         insensitive="#98A584"
+#         insensitive_dark="#3F4D28"
+#         ;;
+#     olive)
+#         accent="#4B8501"
+#         hover="#51850F"
+#         active="#368501"
+#         insensitive="#79856A"
+#         insensitive_dark="#2B4D01"
+#         ;;
+#     prussiangreen)
+#         accent="#308280"
+#         hover="#3D8280"
+#         active="#307682"
+#         insensitive="#688281"
+#         insensitive_dark="#1C4D4B"
+#         ;;
+#     purple)
+#         accent="#7764D8"
+#         hover="#8979D8"
+#         active="#8B64D8"
+#         insensitive="#B4ADD8"
+#         insensitive_dark="#2A234D"
+#         ;;
+#     red)
+#         accent="#DA3450"
+#         hover="#DA4A62"
+#         active="#DA3434"
+#         insensitive="#DAAEB6"
+#         insensitive_dark="#4D121C"
+#         ;;
+#     sage)
+#         accent="#657B69"
+#         hover="#6F8773"
+#         active="#657B6D"
+#         insensitive="#7A877C"
+#         insensitive_dark="#3F4D41"
+#         ;;
+#     viridian)
+#         accent="#03875B"
+#         hover="#10875F"
+#         active="#038771"
+#         insensitive="#6C877E"
+#         insensitive_dark="#024D34"
+#         ;;
+#   esac
+#   cd "${YARU_DEV}/yaru-dirty/"
+#   mkdir -p gtk/src/${THEME}/gtk-2.0/
+#   mkdir -p gtk/src/${THEME}-dark/gtk-2.0/
+#   rsync -a --delete gtk/src/default/gtk-2.0/ gtk/src/${THEME}/gtk-2.0/
+#   rsync -a --delete gtk/src/dark/gtk-2.0/ gtk/src/${THEME}-dark/gtk-2.0/
+
+#   # DEFAULT
+#   sed -i "s|#E95420|${accent}|g" gtk/src/${THEME}/gtk-2.0/gtkrc
+#   sed -i "s|#19B6EE|${accent}|g" gtk/src/${THEME}/gtk-2.0/gtkrc     #link
+#   sed -i "s|#0b7196|${active}|g" gtk/src/${THEME}/gtk-2.0/gtkrc     #visited link
+
+#   # Accent
+#   sed -i "s|#f85731|${accent}|g" gtk/src/${THEME}/gtk-2.0/assets.svg
+#   sed -i "s|#f85731|${accent}|g" gtk/src/${THEME}/gtk-2.0/assets-external.svg
+#   # Hover
+#   sed -i "s|#f96640|${hover}|g" gtk/src/${THEME}/gtk-2.0/assets-external.svg
+#   # Active
+#   sed -i "s|#e55730|${active}|g" gtk/src/${THEME}/gtk-2.0/assets.svg
+#   sed -i "s|#c34113|${active}|g" gtk/src/${THEME}/gtk-2.0/assets.svg
+#   sed -i "s|#c74729|${active}|g" gtk/src/${THEME}/gtk-2.0/assets-external.svg
+#   # Insensitive
+#   sed -i "s|#fea691|${insensitive}|g" gtk/src/${THEME}/gtk-2.0/assets.svg
+#   sed -i "s|#f1a78d|${insensitive}|g" gtk/src/${THEME}/gtk-2.0/assets.svg
+#   sed -i "s|#f6b6a0|${insensitive}|g" gtk/src/${THEME}/gtk-2.0/assets.svg
+#   sed -i "s|#f1a78d|${insensitive}|g" gtk/src/${THEME}/gtk-2.0/assets-external.svg
+
+#   cd gtk/src/${THEME}/gtk-2.0
+#   ./render-all-assets.sh
+#   ./render-assets-external.sh
+
+#   cd "${YARU_DEV}/yaru-dirty/"
+#   # DARK
+#   sed -i "s|#E95420|${accent}|g" gtk/src/${THEME}-dark/gtk-2.0/gtkrc
+#   sed -i "s|#2194bd|${accent}|g" gtk/src/${THEME}-dark/gtk-2.0/gtkrc     #link
+#   sed -i "s|#c97c5b|${active}|g" gtk/src/${THEME}-dark/gtk-2.0/gtkrc     #visited link
+
+#   # Accent
+#   sed -i "s|#f85731|${accent}|g" gtk/src/${THEME}-dark/gtk-2.0/assets.svg
+#   sed -i "s|#f85731|${accent}|g" gtk/src/${THEME}-dark/gtk-2.0/assets-external.svg
+#   # Hover
+#   sed -i "s|#f96640|${hover}|g" gtk/src/${THEME}-dark/gtk-2.0/assets-external.svg
+#   # Active
+#   sed -i "s|#e55730|${active}|g" gtk/src/${THEME}-dark/gtk-2.0/assets.svg
+#   sed -i "s|#c34113|${active}|g" gtk/src/${THEME}-dark/gtk-2.0/assets.svg
+#   sed -i "s|#c74729|${active}|g" gtk/src/${THEME}-dark/gtk-2.0/assets-external.svg
+#   # Insensitive
+#   sed -i "s|#bf6a57|${insensitive}|g" gtk/src/${THEME}-dark/gtk-2.0/assets.svg
+#   sed -i "s|#bf6a5f|${insensitive}|g" gtk/src/${THEME}-dark/gtk-2.0/assets.svg
+#   sed -i "s|#b56d54|${insensitive}|g" gtk/src/${THEME}-dark/gtk-2.0/assets.svg
+#   sed -i "s|#92442e|${insensitive_dark}|g" gtk/src/${THEME}-dark/gtk-2.0/assets-external.svg
+#   cd gtk/src/${THEME}-dark/gtk-2.0
+#   ./render-all-assets.sh
+#   ./render-assets-external.sh
+# done
+
+cd "${YARU_DEV}/yaru-dirty/"
 meson build --prefix="${YARU_NEW}"
 ninja -C build
 ninja -C build install > /dev/null
 
 #icons
+rsync -aHAWXx --delete "${YARU_NEW}/share/icons/Yaru-mate/" "${YARU_NEW}/share/icons/Yaru-MATE-light/"
+rsync -aHAWXx --delete "${YARU_NEW}/share/icons/Yaru-mate-dark/" "${YARU_NEW}/share/icons/Yaru-MATE-dark/"
+sed -i "s|Inherits=Yaru-mate|Inherits=Yaru-MATE-light|" "${YARU_NEW}/share/icons/Yaru-MATE-dark/index.theme"
 for THEME in light dark; do
-    rsync -aHAWXx "${YARU_NEW}/share/icons/Yaru-mate/" "${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/"
-    for RES in 16 22 24; do
-        rsync -aHAWXx "${YARU_NEW}/share/icons/Yaru/${RES}x${RES}/panel/" "${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/${RES}x${RES}/panel/"
-        if [ ${RES} -ne 16 ]; then
-            rsync -aHAWXx "${YARU_NEW}/share/icons/Yaru/${RES}x${RES}/animations/" "${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/${RES}x${RES}/animations/"
-        fi
-    done
-    sed -i "s|Yaru-mate|Yaru-MATE-${THEME}|g" "${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/index.theme"
-    echo "Icons: ${THEME}"
-
-    # Make panel icons dark on the light theme
-    if [ "${THEME}" == "light" ]; then
-        echo " - Patching panel icons"
-        sed -i 's|"#fff"|"#333"|g' ${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/*/panel/*.svg
-        sed -i 's|"#ffffff"|"#3D3D3D"|g' ${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/*/panel/*.svg
-        sed -i 's|"#F9F9F9"|"#3D3D3D"|g' ${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/*/panel/*.svg
-        sed -i 's|fill:#fff;|fill:#333;|g' ${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/*/panel/*.svg
-        sed -i 's|fill:#ffffff;|fill:#3D3D3D;|g' ${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/*/panel/*.svg
-        sed -i 's|"#fff"|"#333"|g' ${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/*/animations/*.svg
-    fi
-    rsync -aHAWXx \
-        "${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/" "${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/icons/Yaru-MATE-${THEME}/"
-
-    update-icon-caches "${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/icons/Yaru-MATE-${THEME}/"
+    sed -i "s|Name=Yaru-mate|Name=Yaru-MATE|" "${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/index.theme"
+    rsync -aHAWXx --delete "${YARU_NEW}/share/icons/Yaru-MATE-${THEME}/" "${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/icons/Yaru-MATE-${THEME}/"
 done
 
 #themes
-rsync -aHAWXx --delete --exclude=metacity-1 "${YARU_NEW}/share/themes/Yaru-mate/" "${YARU_NEW}/share/themes/Yaru-MATE-light/"
-rsync -aHAWXx --delete --exclude=metacity-1 "${YARU_NEW}/share/themes/Yaru-mate-dark/" "${YARU_NEW}/share/themes/Yaru-MATE-dark/"
+rsync -aHAWXx --delete "${YARU_NEW}/share/themes/Yaru-mate/" "${YARU_NEW}/share/themes/Yaru-MATE-light/"
+rsync -aHAWXx --delete "${YARU_NEW}/share/themes/Yaru-mate-dark/" "${YARU_NEW}/share/themes/Yaru-MATE-dark/"
+sed -i "s|Yaru-mate|Yaru-MATE-light|g" "${YARU_NEW}/share/themes/Yaru-MATE-light/index.theme"
+sed -i "s|Yaru-mate-dark|Yaru-MATE-dark|g" "${YARU_NEW}/share/themes/Yaru-MATE-dark/index.theme"
 
-for THEME in light dark; do
-    case ${THEME} in
-      light) METACITY_THEME="Yaru";;
-      dark) METACITY_THEME="Yaru-dark";;
-    esac
-
-    echo "[X-GNOME-Metatheme]
-Name=Yaru-MATE-${THEME}
-Type=X-GNOME-Metatheme
-Comment=Ubuntu MATE Yaru-${THEME} theme
-Encoding=UTF-8
-GtkTheme=Yaru-MATE-${THEME}
-MetacityTheme=${METACITY_THEME}
-IconTheme=Yaru-MATE-${THEME}
-CursorTheme=Yaru
-CursorSize=24
-ButtonLayout=:minimize,maximize,close" > "${YARU_NEW}/share/themes/Yaru-MATE-${THEME}/index.theme"
-    echo "Theme: ${THEME}"
-    rsync -aHAWXx --delete --exclude="gtk-2.0/assets" "${YARU_NEW}/share/themes/Yaru-MATE-${THEME}/" "${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/themes/Yaru-MATE-${THEME}/"
-done
+# TODO: Drop gtk-2.0 exclusion when merged #
+rsync -aHAWXx --delete --exclude="gtk-2.0/assets" "${YARU_NEW}/share/themes/Yaru-MATE-light/" "${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/themes/Yaru-MATE-light/"
+rsync -aHAWXx --delete --exclude="gtk-2.0/assets" "${YARU_NEW}/share/themes/Yaru-MATE-dark/" "${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/themes/Yaru-MATE-dark/"
 
 cd "${YARU_DEV}/ubuntu-mate-artwork-dirty"
-dch -v 22.04.9~jammy$(date +%y\.%j\.%H%M) --distribution jammy "Sync Yaru-MATE themes/icons with upstream Yaru."
+dch -v 22.04.10~jammy$(date +%y\.%j\.%H%M) --distribution jammy "Sync Yaru-MATE themes/icons with upstream Yaru."
 echo
 head -n9 debian/changelog
 echo
 echo "${YARU_DEV}/ubuntu-mate-artwork-dirty"
-for THEME in Yaru-MATE-light Yaru-MATE-dark; do
-  sudo rsync -aHAWXx --delete ${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/themes/${THEME}/ /usr/share/themes/${THEME}/
-  sudo rsync -aHAWXx ${YARU_DEV}/ubuntu-mate-artwork-dirty/usr/share/icons/${THEME}/ /usr/share/icons/${THEME}/
+
+for THEME in Yaru Yaru-dark; do
+  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/themes/${THEME}/ /usr/share/themes/${THEME}/
+  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/icons/${THEME}/ /usr/share/icons/${THEME}/
+  sudo gtk-update-icon-cache -f /usr/share/icons/${THEME}
 done
-#for THEME in Yaru Yaru-bark Yaru-blue Yaru-magenta Yaru-mate Yaru-olive Yaru-prussiangreen Yaru-purple Yaru-red Yaru-sage Yaru-viridian; do
+
+#for THEME in Yaru-MATE-light Yaru-MATE-dark; do
 #  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/themes/${THEME}/ /usr/share/themes/${THEME}/
-#  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/themes/${THEME}-dark/ /usr/share/themes/${THEME}-dark/
 #  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/icons/${THEME}/ /usr/share/icons/${THEME}/
+#  sudo gtk-update-icon-cache -f /usr/share/icons/${THEME}
 #done
+
+for THEME in Yaru-bark Yaru-blue Yaru-magenta Yaru-mate Yaru-olive Yaru-prussiangreen Yaru-purple Yaru-red Yaru-sage Yaru-viridian; do
+  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/themes/${THEME}/ /usr/share/themes/${THEME}/
+  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/icons/${THEME}/ /usr/share/icons/${THEME}/
+  sudo gtk-update-icon-cache -f /usr/share/icons/${THEME}
+  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/themes/${THEME}-dark/ /usr/share/themes/${THEME}-dark/
+  sudo rsync -aHAWXx --delete ${YARU_NEW}/share/icons/${THEME}-dark/ /usr/share/icons/${THEME}-dark/
+  sudo gtk-update-icon-cache -f /usr/share/icons/${THEME}-dark
+done
